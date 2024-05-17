07c12c7762b431fb242164331dff54bbe8d2349f
Sobanggg <abolfazleshghi1385@gmail.com>
2024-05-17 15:22:06 -0400

    add port scan

--- liveSystem.sh
+++ liveSystem.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 read -p "enter the range ip: " rIp 
 read -p "enter the number of ips: " num 
+read -p "enter the number of port for scan: " numP 
 declare -a ips
 rIp=$(echo $rIp | cut -d . -f 1,2,3)
 
 for ip in $(seq 1 $num); do
     ping=$(ping -c 1 $rIp.$ip | grep "bytes from" | awk -F " " '{print $4}' | sed 's/://g')
-    ips[$ip]="$ping"
+    #ips[$ip]="$ping"
+    if [[  $ping != "" ]]; then
+    for port in $(seq 1 $numP); do 
+	    echo "" > /dev/tcp/$ping/$port && echo "Port is Open"|grep "Port is Open"
+    done
+    fi
 done
 
-echo ${ips[@]}
+#echo ${ips[@]}
 
